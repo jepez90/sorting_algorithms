46347ecebebfe6fa81b901dafbefa6810114ce31
jepez90 <ing.jersonperez@gmail.com>
2021-06-15 15:15:19 -0500

    [Update] refactor quick_sort function

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -17,6 +17,23 @@ void quick_sort(int *array, size_t size)
 	_quick_sort(array, size, array, size);
 }
 
+/**
+ * array_swap - sorts an array of integers in ascending order using the Quick
+ * sort algorithm
+ * @array: Array of integers to be used.
+ * @first: Size of the array.
+ * @second: Size of the array.
+ * Return: Nothing.
+ */
+void array_swap(int *array, size_t first, size_t second)
+{
+	int hold;
+
+	hold = array[second];
+	array[second] = array[first];
+	array[first] = hold;
+}
+
 
 /**
  * _quick_sort - sorts an array of integers in ascending order using
@@ -29,43 +46,42 @@ void quick_sort(int *array, size_t size)
  */
 void _quick_sort(int *array, size_t size, int *sub_array, size_t sub_size)
 {
-	size_t i = 0, j = 1;
-	int pivot, temp;
+	int hold, pivot = sub_array[sub_size - 1];
+	size_t i = 0, j;
 
-	pivot = sub_array[sub_size - 1];
-	while (i < sub_size)
+	while (sub_array[i++] < pivot)
 	{
-		if (sub_array[i] < pivot)
+		if (i == (sub_size - 1))
+			_quick_sort(array, size, sub_array, sub_size - 1);
+	}
+	j = i + 1;
+	while (j < sub_size)
+	{
+		if (sub_array[j] < pivot)
 		{
-			i++;
+			if (sub_array[i] != sub_array[j])
+			{
+				array_swap(sub_array, i, j);
+				print_array(array, size);
+			}
 			j++;
-			continue;
+			i++;
 		}
 		else
 		{
-			while (sub_array[j] >= pivot && j < (sub_size - 1))
-				j++;
-
-			if (j < sub_size && sub_array[i] != sub_array[j])
+			if (j == (sub_size - 1) && sub_array[i] != sub_array[j])
 			{
-				temp = sub_array[j];
-				sub_array[j] = sub_array[i];
-				sub_array[i] = temp;
+				array_swap(sub_array, i, j);
 				print_array(array, size);
 			}
-			else
-				j--;
+			j++;
 		}
-		if (j == sub_size - 1)
+		if (j == sub_size)
 		{
 			if (i > 1)
-			{
 				_quick_sort(array, size, sub_array, i);
-			}
 			if (i < sub_size - 2)
-			{
 				_quick_sort(array, size, sub_array + i + 1, sub_size - i - 1);
-			}
 			break;
 		}
 	}
