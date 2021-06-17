3ad61686e035a09432bc809b26d4e3e10deb5c3e
Carolinacapote <carolina.capote28@gmail.com>
2021-06-17 20:49:59 +0000

    Counting sort

---
+++ 102-counting_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+/**
+ * counting_sort - sorts an array of integers in ascending order using the
+ * Counting sort algorithm.
+ * @array: Array to be sorted.
+ * @size: of the array.
+ * Return: Nothing.
+ */
+void counting_sort(int *array, size_t size)
+{
+	int i, index, idx_position, value, max_number = array[0];
+	int *array_count, *array_copy;
+
+	for (i = 1; i < (int)size; i++)
+	{
+		if (array[i] > max_number)
+			max_number = array[i];
+	}
+
+	array_count = malloc(sizeof(*array_count) * (max_number + 1));
+	if (!array_count)
+		return;
+
+	for (i = 0; i <= max_number; i++)
+		array_count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+	{
+		index = array[i];
+		array_count[index] += 1;
+	}
+	for (i = 0; i <= max_number; i++)
+		array_count[i] += array_count[i - 1];
+
+	print_array(array_count, max_number + 1);
+
+	array_copy = malloc(sizeof(*array_copy) * size);
+	if (!array_copy)
+		return;
+
+	for (i = 0; i < (int)size; i++)
+		array_copy[i] = array[i];
+	for (i = size - 1; i >= 0; i--)
+	{
+		value = array_copy[i];
+		idx_position = --(array_count[value]);
+		array[idx_position] = value;
+	}
+}
